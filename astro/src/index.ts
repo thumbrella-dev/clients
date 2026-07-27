6c8e7dd51098db877070904ac583190bfebfdf5f
Peter Shinners <pete@shinners.org>
2026-07-26 19:57:31 -0600

    remove conceptual react and astro components

--- astro/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { initThumbnails } from "./browser.js";
-export { default as Thumbnail } from "./Thumbnail.astro";
-export { default as Thumbrella } from "./Thumbrella.astro";
--- react/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { Thumbnail } from "./Thumbnail.js";
-export type { ThumbnailProps } from "./Thumbnail.js";
-export { Thumbrella } from "./Thumbrella.js";
-export type { ThumbrellaProps } from "./Thumbrella.js";
-export { initThumbnails } from "./browser.js";
